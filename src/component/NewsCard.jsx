502d57403c526df53b2cc07f68990b82210e99ea
William Klaiwonpen <williamklaiwonpen@gmail.com>
2026-01-09 18:44:12 +0700

    Final Version

--- src/component/NewsCard.jsx
+++ src/component/NewsCard.jsx
@@ -1,7 +1,7 @@
 const NewsCard = () => {
   return (
     <>
-      <div className="bg-white rounded-xl mx-6 px-2">
+      <div className="bg-white rounded-xl mx-6 px-2 shadow-sm">
         <div className="flex flex-col md:items-center md:flex-row p-2">
           {/* Image Container */}
           <img
@@ -12,11 +12,12 @@ const NewsCard = () => {
           {/* Text Container */}
           <div>
             {/* Heading Text */}
-            <div className="py-6 md:ml-6">
+
+            <div className="py-6 md:ml-10 ">
               <h1 className="text-Dark-Lavender font-poppins font-bold text-2xl">
                 Get Updated!
               </h1>
-              <p className="max-w-[300px] mt-3 text-black font-poppins text-xs font-light">
+              <p className="max-w-[250px] mt-3 text-black font-poppins text-xs font-light">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s.
@@ -25,13 +26,17 @@ const NewsCard = () => {
               {/* Form Container */}
               <div className="mt-6">
                 <p className="font-poppins text-xs">Email address</p>
-                <form action="">
+                <form action="" onSubmit={(e) => e.preventDefault()}>
                   <input
-                    type="text"
+                    type="email"
+                    required
                     className="mt-1 px-1 py-2 w-full border border-gray-100 rounded-sm placeholder:font-poppins placeholder:text-xs focus:outline-Dark-Lavender"
                     placeholder="moe@example.com"
                   />
-                  <button className="bg-Medium-Lavender px-1 py-2 w-full mt-2 md:mt-4 rounded-md font-poppins font-light text-white text-sm transition hover:bg-Dark-Lavender duration-300 cursor-pointer">
+                  <button
+                    type="submit"
+                    className="bg-Medium-Lavender px-1 py-2 w-full mt-2 md:mt-4 rounded-md font-poppins font-light text-white text-sm transition hover:bg-Dark-Lavender duration-300 cursor-pointer"
+                  >
                     Subscribe now
                   </button>
                 </form>
